64926b53e618f478bd270793673029cd2d23c8c3
Zanna Fitch <zannafitch2004@gmail.com>
2023-06-05 10:31:23 -0500

    add finished iteration 1 tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,7 +3,7 @@ require './lib/ride'
 
 
 RSpec.describe Ride do
-  it "exists and has attributes/cells" do
+  it "exists and has attributes" do
     ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
 
     expect(ride1).to be_an_instance_of(Ride)
@@ -14,7 +14,8 @@ RSpec.describe Ride do
     expect(ride1.loop?).to eq(false)
     expect(ride1.total_distance).to eq(21.4)
 
-
-    
+    ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    expect(ride2.loop?).to eq(true)
+    expect(ride2.total_distance).to eq(14.9)
   end
 end
